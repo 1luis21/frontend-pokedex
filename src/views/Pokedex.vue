0d870ddf901cd8848077be9cd0a282b55fa798e5
Luis <lmontano@ucol.mx>
2020-06-11 00:06:24 -0500

    third commit

--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -32,40 +32,40 @@
                     <input type="text" class="form-control" placeholder="Legendario" v-model="caractPokemon.legendario">
                 </div>
             </div>
-            <button class="btn btn-primary btn-sm mx-2 float-left" type="submit">Agregar</button>
-            <button class="btn btn-info btn-sm float-left">Actualizar</button> <br>
+            <button class="btn btn-primary btn-sm mx-2 float-left" type="submit">Agregar</button><br>
         </form>
 
-        <form @submit.prevent="editarPokemon()" v-if="editar">
+        <form @submit.prevent="editarPokemon(pokemonEditar)" v-else>
             <h3 class="text-warning text-left">Editar PoKéMoN</h3>
             <div class="col-4 ml-4">
                 <div class="form-group"> 
-                    <input type="text" class="form-control" placeholder="Número en Pokédex" v-model="caractPokemon.numdex">
+                    <input type="text" class="form-control" placeholder="Número en Pokédex" v-model="pokemonEditar.numdex">
                 </div>
 
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Nombre" v-model="caractPokemon.nombre">
+                    <input type="text" class="form-control" placeholder="Nombre" v-model="pokemonEditar.nombre">
                 </div>
 
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Tipo" v-model="caractPokemon.tipo">
+                    <input type="text" class="form-control" placeholder="Tipo" v-model="pokemonEditar.tipo">
                 </div>
 
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Debilidad" v-model="caractPokemon.debilidad">
+                    <input type="text" class="form-control" placeholder="Debilidad" v-model="pokemonEditar.debilidad">
                 </div>
 
 
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Region" v-model="caractPokemon.region">
+                    <input type="text" class="form-control" placeholder="Region" v-model="pokemonEditar.region">
                 </div>
 
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Legendario" v-model="caractPokemon.legendario">
+                    <input type="text" class="form-control" placeholder="Legendario" v-model="pokemonEditar.legendario">
                 </div>
             </div>
-            <button class="btn btn-info btn-sm mx-2 float-left" type="submit">Cancelar</button>
-            <button class="btn btn-warning btn-sm float-left">Actualizar</button> <br>
+            
+            <button class="btn btn-warning btn-sm mx-2 float-left" type="submit">Actualizar</button>
+            <button class="btn btn-info btn-sm mx-2 float-left" type="submit" @click="editar = false">Cancelar</button><br>
         </form>
 
 
@@ -96,7 +96,7 @@
                      <td class="text-center">
                         <button @click="activarEdicion(item._id)" class="btn btn-light btn-sm">Editar</button>
                         <p></p>
-                        <button @click="eliminarNota(item._id)" class="btn btn-danger btn-sm">Eliminar</button>
+                        <button @click="eliminarPokemon(item._id)" class="btn btn-danger btn-sm">Eliminar</button>
                      </td>
                 </tr>
             </tbody>
@@ -108,9 +108,10 @@
 export default {
     data() {
         return {
-            Pokemons:[],
             caractPokemon: {numdex: '', nombre: '', tipo: '', debilidad: '', region: '', legendario: ''},
-            editar: false
+            editar: false,
+            pokemonEditar: [],
+            Pokemons:[]
         }
     },
     created() {
@@ -151,7 +152,7 @@ export default {
                 swal('Invalido', 'Elemento no agregado','warning');
             })
         },
-        eliminarNota(id){
+        eliminarPokemon(id) { 
             this.axios.delete(`/pokemon/${id}`)
             .then(res => {
                 let index = this.Pokemons.findIndex( item => item._id === res.data._id )
@@ -172,12 +173,42 @@ export default {
                 } else {
 
                 }
+                })
             })
-    })
-    .catch( e => {
-      console.log(e.response);
-    })
-},
+            .catch( e => {
+                console.log(e.response);
+                })
+        },
+        activarEdicion(id) {
+            this.editar = true;
+            console.log(id);
+            this.axios.get(`pokemon/${id}`)
+            .then(res => {
+                this.pokemonEditar = res.data;
+            })
+            .catch(e => {
+                console.log(e.response);
+            })
+        },
+        editarPokemon(item) {
+            this.axios.put(`/pokemon/${item._id}`, item)
+            .then(res => {
+                let index = this.Pokemons.findIndex(p => p._id === res.data._id)
+                this.Pokemons[index].numdex = res.data.numdex;
+                this.Pokemons[index].nombre = res.data.nombre;
+                this.Pokemons[index].tipo = res.data.tipo;
+                this.Pokemons[index].debilidad = res.data.debilidad;
+                this.Pokemons[index].region = res.data.region;
+                this.Pokemons[index].legendario = res.data.legendario;
+                this.pokemonEditar = {};
+                swal('Actualizado', 'Nuevo elemento actualizado','success');
+                this.editar = false;
+            })
+            .catch(e => {
+                console.log(e.response);
+            })
+        }
+
     }
 }
 </script>
